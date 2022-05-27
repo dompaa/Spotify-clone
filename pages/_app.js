909b54c22c215d5d5e9014f43b7930c797900023
Dominik <gradiskid@gmail.com>
2022-05-27 23:38:30 +0200

    add SessionProvider HOC

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps} }) {
+  
+  return (
+    //HOC - higher order components
+  <SessionProvider session={session}>
+    <Component {...pageProps} />
+  </SessionProvider>
+  
+  )
 }
 
 export default MyApp
