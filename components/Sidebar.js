506a6f92118e0b3d0574223f21c9e560e87a48cf
Dominik <gradiskid@gmail.com>
2022-06-02 02:22:15 +0200

    add playlist integration with the user

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,9 +8,26 @@ import {
     RssIcon,
 } from "@heroicons/react/outline"
 import { signOut, useSession } from 'next-auth/react'
+import { useState, useEffect } from 'react';
+import useSpotify from '../hooks/useSpotify';
+
+
+
 
 function Sidebar() {
+    const spotifyApi = useSpotify();
     const { data: session, status} = useSession();
+    const [playlists, setPlaylists] = useState([]);    
+
+    useEffect(() => {
+        if (spotifyApi.getAccessToken()) {
+            spotifyApi.getUserPlaylists().then((data) => {
+                setPlaylists(data.body.items);
+            });
+        }
+    },[session, spotifyApi])
+
+    console.log(playlists);
 
    // console.log(session);
   return (
@@ -48,33 +65,15 @@ function Sidebar() {
         <hr className='border-t-[0.1px] border-gray-900'/>
 
        {/* Playlists... */}
-       <p className='cursor-pointer hover:text-white'>
-           Playlist name...
-       </p>
-       <p className='cursor-pointer hover:text-white'>
-           Playlist name...
-       </p>
-       <p className='cursor-pointer hover:text-white'>
-           Playlist name...
-       </p>
-       <p className='cursor-pointer hover:text-white'>
-           Playlist name...
-       </p>
-       <p className='cursor-pointer hover:text-white'>
-           Playlist name...
-       </p>
-       <p className='cursor-pointer hover:text-white'>
-           Playlist name...
-       </p>
-       <p className='cursor-pointer hover:text-white'>
-           Playlist name...
-       </p>
-       <p className='cursor-pointer hover:text-white'>
-           Playlist name...
-       </p>
+        {playlists.map((playlist) => (
+        //everytime when iteration you need a key
+       <p key={playlist.id} className='cursor-pointer hover:text-white'>
+           {playlist.name}
+        </p>
+        ))}
         </div>
     </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
