2c287d796755fbac8e1a54024acd3e783357fc1e
Dominik <gradiskid@gmail.com>
2022-06-02 18:24:01 +0200

    add prep for recoil

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps: { session, ...pageProps} }) {
   
   return (
     //HOC - higher order components
   <SessionProvider session={session}>
+      <RecoilRoot>
     <Component {...pageProps} />
+      </RecoilRoot>
   </SessionProvider>
   
   )
