3c4b5054b5128cb4181313f818e29a5bdc4493c5
Dominik <gradiskid@gmail.com>
2022-06-08 23:12:48 +0200

    add styling for each song

--- components/Songs.js
+++ components/Songs.js
@@ -7,7 +7,7 @@ function Songs() {
     //access to values
     const playlist = useRecoilValue(playlistState)
     return (        
-    <div className="text-white flex flex-col space-y-1 pb-28 ">
+    <div className="text-white flex flex-col space-y-1 pb-28">
         {playlist?.tracks.items.map((track, i) => (
            <Song key={track.track.id} track={track} order={i}/>
         ))} 
