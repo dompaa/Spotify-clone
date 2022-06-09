6ad01828fb3f2010545a5d619707b286274b00aa
Dominik <gradiskid@gmail.com>
2022-06-09 23:45:24 +0200

    add watcher for current track/state

---
+++ atoms/songAtom.js
@@ -0,0 +1,13 @@
+import { atom } from "recoil";
+
+
+//what track is selected
+export const currentTrackIdState = atom({
+    key: "currentTrackIdState", //unique ID (with respect to other atoms/selectors)
+    default: null, // default value (aka initial value)
+});
+
+export const isPlayingState = atom ({
+    key: "isPlayingState",
+    default: false,
+})
