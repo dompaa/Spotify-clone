603ff17e2e7b8967008eb6cb791c686664e80fff
Dominik <gradiskid@gmail.com>
2022-06-13 02:34:11 +0200

    add fetching song info

--- components/Player.js
+++ components/Player.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useSession } from 'next-auth/react';
+import React, { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil';
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
+import useSongInfo from '../hooks/useSongInfo';
+import useSpotify from '../hooks/useSpotify'
 
 function Player() {
+    //all states preping
+    const spotifyApi = useSpotify();
+    const { data: session, status} = useSession();
+    const [currentTrackId, setCurrentIdTrack] = useRecoilState(currentTrackIdState);
+    const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+    const [volume, setVolume] = useState(50);
+
+
+    const songInfo = useSongInfo();
+
+    //function
+    const fetchCurrentSong = () => {
+        if (!songInfo) {
+            spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+                console.log("Now playing: ", data.body?.item);
+                setCurrentIdTrack(data.body?.item?.id);
+
+                spotifyApi.getMyCurrentPlaybackState().then((data) => {
+                    setIsPlaying(data.body?.is_playing);
+                });
+            });
+        }
+    }
+
+    useEffect(() => {
+        if(spotifyApi.getAccessToken() && !currentTrackId)
+        {
+            // fetch the song info
+            fetchCurrentSong();
+            setVolume(50);
+        }
+    }, [currentTrackIdState, spotifyApi, session]);
+
   return (
     <div>
-        <h1>player</h1>
+        {/* Left */}
+        <div>
+            <img className='hidden md:inline h-10 w-10' src={songInfo?.album.images?.[0]?.url} alt="" />
+        </div>
     </div>
   )
 }
